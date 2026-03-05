9fea6b9ad9a87059a1e02277dad4e8ca9b52c168
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-03-05 07:57:51 +0530

    Add files via upload

---
+++ Drinks.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	int n;
+	cin>>n;
+	
+	double sum;
+	for(int i=0; i<n; i++){
+		double p;
+		cin>>p;
+		sum += p;
+	}
+	
+	double result = sum/n;
+	
+	cout<<result<<setprecision(12);
+	return 0;
+}
