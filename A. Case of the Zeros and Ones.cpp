784a2bf76124f76ddc8baccbc9d416e5dde9233a
Deepikagautam993 <gautamdeepika707@gmail.com>
2026-05-13 07:53:19 +0530

    Add files via upload

---
+++ A. Case of the Zeros and Ones.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    
+    int zeros = 0, ones = 0;
+    for (char c : s) {
+        if (c == '0') zeros++;
+        else ones++;
+    }
+    
+    cout << abs(zeros - ones) << endl;
+    
+    return 0;
+}
